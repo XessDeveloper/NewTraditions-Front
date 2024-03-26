7ce346bf81fea0385f990c24860e4751ea24fb6e
xessdeveloper <xessdeveloperstudent@gmail.com>
2024-03-26 20:32:52 +0100

    view plates & crud plates structure created

---
+++ src/app/Views/plates/crud-plates/crud-plates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-plates',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-plates.component.html',
+  styleUrl: './crud-plates.component.scss'
+})
+export class CrudPlatesComponent {
+
+}
