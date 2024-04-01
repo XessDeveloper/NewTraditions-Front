6de4fe98f45537b363d80f1d5b7af5a220857f0f
Xess <xessdeveloperstudent@gmail.com>
2024-04-01 14:07:35 +0200

    Changed footer and added temp image

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+//TODO => Responsive
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,16 +9,15 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   email: string = 'prueba@empresa.com'
-  localization: string = 'Calle Prueba, 52'
+  localization: string = 'Calle Prueba, 52 - San Vicente del Raspeig'
   phone: string = '+34 123456789'
   timetable: string[] = [
-    'Lunes: 08AM - 17PM',
-    'Martes: 08AM - 17PM',
-    'Miércoles: 08AM - 17PM',
-    'Jueves: 08AM - 17PM',
-    'Viernes: 08AM - 17PM',
-    'Sábado: 08AM - 17PM',
-    'Domingo: 08AM - 17PM',
-
+    'Lunes: 08:00 - 17:30',
+    'Martes: 08:00 - 17:45',
+    'Miércoles: 08:30 - 17:00',
+    'Jueves: 12:00 - 16:00',
+    'Viernes: 09:30 - 12:00',
+    'Sábado: 10:45 - 13:30',
+    'Domingo: Cerrado',
   ]
 }
