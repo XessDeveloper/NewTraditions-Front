f43d53800eb0ac0f700bd011f330ff25f3b06091
xessdeveloper <xessdeveloperstudent@gmail.com>
2024-04-10 22:29:05 +0200

    Changed cookies for JWT & added ingredientes/platos routing

--- src/app/Services/auth/auth.service.ts
+++ src/app/Services/auth/auth.service.ts
@@ -2,36 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IAuthUser } from '../../Models/Auth/i-auth-user';
-import { IStatusCodeResponse } from '../../Models/shared/i-status-code-response';
+import { IApiResponse } from '../../Models/shared/i-api-response';
 import { CookieService } from 'ngx-cookie-service';
 import { IAuthRequest } from '../../Models/Auth/requests/i-auth-request';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({ providedIn: 'root' })
 
 export class AuthService {
-  constructor(private http: HttpClient, private cookieService: CookieService) { }
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
 
-  Register(request: IAuthRequest): Observable<IStatusCodeResponse<IAuthRequest>> {
-    return this.http.post<IStatusCodeResponse<IAuthRequest>>('/Auth/Register', request);
+  Register(request: IAuthRequest): Observable<IApiResponse<IAuthRequest>> {
+    return this.http.post<IApiResponse<IAuthRequest>>('/Auth/Register', request);
   }
 
-  Login(request: IAuthUser): Observable<IStatusCodeResponse<IAuthUser>> {
-     return this.http.post<IStatusCodeResponse<IAuthUser>>('/Auth/Login', request);
+  Login(request: IAuthUser): Observable<IApiResponse<IAuthUser>> {
+     return this.http.post<IApiResponse<IAuthUser>>('/Auth/Login', request);
   }
 
-  async GetCookie() {
-    let cookie: string = '';
+  GetFromLocal = () => localStorage.getItem('token');
+  SetToLocal = (tokenJWT: string) => localStorage.setItem('token', tokenJWT);
+  CheckIfUserLogged = () => !!this.GetFromLocal()
+  GetClaimsFromToken = () => {
+    const token = this.GetFromLocal();
 
-    while (cookie === '') {
-      cookie = this.cookieService.get(".AspNetCore.Identity.Application")
-
-      if (cookie === '') {
-        await new Promise(resolve => setTimeout(resolve, 100))
-      }
-    }
-  }
-
-  SetCookie(cookieName: string, cookieValue: string) {
-    this.cookieService.set(cookieName, cookieValue);
+    if (token) return this.jwtHelper.decodeToken(token)
+    
+    return null
   }
 }
--- src/app/app-routing.component.ts
+++ src/app/app-routing.component.ts
@@ -3,12 +3,16 @@ import { RegisterComponent } from './Views/register/register.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './Views/login/login.component';
 import { HomeComponent } from './Views/home/home.component';
+import { IngredientsComponent } from './Views/ingredients/ingredients.component';
+import { PlatesComponent } from './Views/plates/plates.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'home', component: HomeComponent, pathMatch: 'full' },
     { path: 'registro', component: RegisterComponent, pathMatch: 'full' },
-    { path: 'login', component: LoginComponent, pathMatch: 'full' }
+    { path: 'login', component: LoginComponent, pathMatch: 'full' },
+    { path: 'ingredientes', component: IngredientsComponent, pathMatch: 'full' },
+    { path: 'platos', component: PlatesComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
