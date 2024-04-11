6cce1335bf220a28d211a202a2b0cab090320398
xessdeveloper <xessdeveloperstudent@gmail.com>
2024-04-11 13:30:53 +0200

    Create Edit/Create ingredient component

---
+++ src/app/Views/ingredients/create-ingredient/create-ingredient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-ingredient',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-ingredient.component.html',
+  styleUrl: './create-ingredient.component.scss'
+})
+export class CreateIngredientComponent {
+
+}
