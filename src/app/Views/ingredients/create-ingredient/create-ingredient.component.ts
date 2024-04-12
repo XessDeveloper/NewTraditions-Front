c08b8460048a8aaa808cce7019ae9a40cd6c4899
xessdeveloper <xessdeveloperstudent@gmail.com>
2024-04-12 20:01:43 +0200

    quit standalone

--- src/app/Views/ingredients/create-ingredient/create-ingredient.component.ts
+++ src/app/Views/ingredients/create-ingredient/create-ingredient.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-ingredient',
-  standalone: true,
-  imports: [],
   templateUrl: './create-ingredient.component.html',
   styleUrl: './create-ingredient.component.scss'
 })
