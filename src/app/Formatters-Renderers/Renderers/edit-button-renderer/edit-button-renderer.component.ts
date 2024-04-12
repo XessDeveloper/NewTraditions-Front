2b24fc78cc1814e17b0e9aaf5283c633fa2034be
xessdeveloper <xessdeveloperstudent@gmail.com>
2024-04-12 20:00:56 +0200

    Created & edit click with IcellRenderers

--- src/app/Formatters-Renderers/Renderers/edit-button-renderer/edit-button-renderer.component.ts
+++ src/app/Formatters-Renderers/Renderers/edit-button-renderer/edit-button-renderer.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
 import { ICellRendererParams } from 'ag-grid-community';
+import { SharedServicesService } from '../../../Services/shared/shared-services.service';
 
 @Component({
   selector: 'app-edit-button-renderer',
@@ -8,8 +10,20 @@ import { ICellRendererParams } from 'ag-grid-community';
   styleUrl: './edit-button-renderer.component.scss'
 })
 export class EditButtonRendererComponent implements ICellRendererAngularComp {
-  params: any;
 
-  agInit(params: ICellRendererParams<any, any, any>) { this.params = params }
+  constructor(private router: Router, private sharedService: SharedServicesService) {}
+
+  @Input() currentUrl: any;
+  paramsRenderer: any;
+
+  editClicked = (itemClicked: any) => {
+    this.sharedService.selectedItemParam = itemClicked;
+    this.router.navigate([`${this.currentUrl}/modificar`])
+  }
+
+  agInit(params: ICellRendererParams<any, any, any>) { 
+    this.paramsRenderer = params 
+    this.currentUrl = this.paramsRenderer.currentUrl  
+  }
   refresh(params: ICellRendererParams<any, any, any>) { return false }
 }
