0d1dda3f61a3e0ac71f7f24e27bbf616e240eed4
xessdeveloper <xessdeveloperstudent@gmail.com>
2024-04-13 15:10:38 +0200

    cancelButtonForm created

--- src/app/HelpMethods/help-methods.ts
+++ src/app/HelpMethods/help-methods.ts
@@ -1,5 +1,6 @@
 import { NgForm } from "@angular/forms";
 import { IIngredient } from "../Models/ingredient/i-ingredient";
+import Swal from "sweetalert2";
 
 export class HelpMethods {
     resetFields(form: NgForm) {
@@ -10,5 +11,17 @@ export class HelpMethods {
         return ingredient.map(ing => {
             return { ...ing, price: ing.price + '€' };
         });
+    }
+
+    cancelButtonForm() {
+        Swal.fire({
+            title: "¿Estas seguro que quieres salir?",
+            showConfirmButton: true,
+            showCancelButton: true,
+            confirmButtonText: "Salir",
+            cancelButtonText: "Cancelar"
+          }).then(result => {
+            if (result.isConfirmed) window.history.back();
+          })
     }
 }
