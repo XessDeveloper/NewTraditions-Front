739da283e5dcc74fc99efe52576b58f83b1dfb61
xessdeveloper <xessdeveloperstudent@gmail.com>
2024-04-22 07:15:02 +0200

    Cambiada la logica para pasar el token o no dependiendo de si esta en el session storage

--- src/app/Interceptors/interceptor-url.interceptor.ts
+++ src/app/Interceptors/interceptor-url.interceptor.ts
@@ -1,22 +1,24 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../Environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { AuthService } from '../Services/auth/auth.service';
 
 @Injectable()
 export class interceptorUrlInterceptor implements HttpInterceptor {
   private apiUrl = environment.apiUrl;
+  private apiUrl2 = environment.apiUrl2;
 
   constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.GetToken();
-
-      const modifiedRequest = request.clone({
+  
+      const modifiedRequestUrl1 = request.clone({
         url: `${this.apiUrl}${request.url}`,
-        setHeaders: { Authorization: `Bearer ${token}`}
+        setHeaders: token !== null ? { Authorization: `Bearer ${token}` } : {}
       });
-      return next.handle(modifiedRequest);
-    }
+
+      return next.handle(modifiedRequestUrl1);
+  }
 }
