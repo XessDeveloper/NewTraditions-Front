6730d932e324fc8dd935da54e1ddbce507590a74
xessdeveloper <xessdeveloperstudent@gmail.com>
2024-04-24 19:04:45 +0200

    delete & edit generic button for grid

--- src/app/Formatters-Renderers/Renderers/delete-button-renderer/delete-button-renderer.component.ts
+++ src/app/Formatters-Renderers/Renderers/delete-button-renderer/delete-button-renderer.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Injector, Input, Output } from '@angular/core';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
 import { ICellRendererParams } from 'ag-grid-community';
+import { IngredientsService } from '../../../Services/ingredients/ingredients.service';
+import { PlatesService } from '../../../Services/plates/plates.service';
+import { HelpMethods } from '../../../HelpMethods/help-methods';
 
 @Component({
   selector: 'app-delete-button-renderer',
@@ -8,8 +11,53 @@ import { ICellRendererParams } from 'ag-grid-community';
   styleUrl: './delete-button-renderer.component.scss'
 })
 export class DeleteButtonRendererComponent implements ICellRendererAngularComp {
-  params: any;
+  private readonly helpMethod = inject(HelpMethods);
+  private readonly injector = inject(Injector);
 
-  agInit(params: ICellRendererParams<any, any, any>) { this.params = params }
-  refresh(params: ICellRendererParams<any, any, any>) { return false }
+  @Input() currentUrl: any;
+  @Output() gridRefresh = new EventEmitter<void>()
+  paramsRenderer: any;
+
+  deleteClicked = (itemClicked: any) => {
+    this.helpMethod.showSwalDelete( () => this.CallDeleteMethodFromCorrectService(itemClicked))
+  }
+
+  CallDeleteMethodFromCorrectService = (itemClicked: any) => {
+    const service = this.getService(this.currentUrl);
+    const Id = itemClicked.id
+
+    if (service) {
+      service.DeleteIngredient(Id).subscribe( (response: any) => {
+            this.helpMethod.showSwalResponses({
+              isSuccessfull: true,
+              message: response.message, 
+              statusCode: response.statusCode,
+              okAction: () => { window.location.reload(); }
+            }) 
+        }
+      ),
+      (errorResponse: any) => {
+        this.helpMethod.showSwalResponses({
+          isSuccessfull: false, 
+          message: errorResponse.error.message, 
+          statusCode: errorResponse.statusCode
+        })
+      }
+    }
+  }
+
+  //En base a la URL actual, obtener el servicio correspondiente 
+  getService(url: string): any {
+    switch(url) {
+      case '/ingredientes': return this.injector.get(IngredientsService);
+      case '/platos': return this.injector.get(PlatesService);
+      default: return null;
+    }
+  }
+
+  agInit(params: ICellRendererParams<any, any, any>) { 
+    this.paramsRenderer = params
+    this.currentUrl = this.paramsRenderer.currentUrl  
+   }
+  refresh(params: ICellRendererParams<any, any, any>) { return true }
 }
